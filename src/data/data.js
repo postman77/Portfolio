f669d1d7d29100c9c274519f3f8c4e1a3b77eb72
postman77 <98200639+postman77@users.noreply.github.com>
2023-01-28 00:52:43 -0500

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -5,21 +5,21 @@ import Comic from '../assets/projects/project3.png'
 export const data=[
     {
         id:1,
-        name:"React JS Application",
+        name:"Workout Music Player",
         image:MusicPlayer,
         github:"https://github.com/postman77/Workout-Music-Player",
         live:"https://postman77.github.io/Workout-Music-Player/",
     },
     {
         id:2,
-        name:"React JS Application",
+        name:"Language Translator",
         image:Translator,
         github:"https://github.com/postman77/Translator",
         live:"https://postman77.github.io/Translator/",
     },
     {
         id:3,
-        name:"React JS Application",
+        name:"Comic Book Encyclopedia",
         image:Comic,
         github:"https://github.com/postman77/ComicBookEncyclopedia.github.io",
         live:"https://postman77.github.io/ComicBookEncyclopedia.github.io/",
